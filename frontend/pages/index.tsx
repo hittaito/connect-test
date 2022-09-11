6d9508710b9e7e346fcc690273c50b326af91e69
hittaito-win <310cifra.ggk@gmail.com>
2022-09-11 10:31:36 +0900

    tailwindcss setup

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,50 +1,51 @@
-import type { NextPage } from "next";
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next'
+import { useState } from 'react'
 import {
   createConnectTransport,
   createPromiseClient,
-} from "@bufbuild/connect-web";
-import { ChatService } from "../schemas/v1/chat_connectweb";
+} from '@bufbuild/connect-web'
+import { ChatService } from '../schemas/v1/chat_connectweb'
 
 const Home: NextPage = () => {
-  const [statement, setStatement] = useState<string>("");
+  const [statement, setStatement] = useState<string>('')
 
   const client = createPromiseClient(
     ChatService,
     createConnectTransport({
-      baseUrl: "http://localhost:8080",
+      baseUrl: 'http://localhost:8080',
     })
-  );
+  )
   const send = async (text: string) => {
     const res = await client.send({
-      id: "1",
-      text: "Hello fron client",
-    });
-    console.log(res);
-  };
+      id: '1',
+      text: 'Hello fron client',
+    })
+    console.log(res)
+  }
   const handleSend = () => {
-    send("aaaa");
-  };
+    send('aaaa')
+  }
 
   const read = async () => {
     for await (const res of client.read({
-      id: "a",
-      text: "server side stream",
+      id: 'a',
+      text: 'server side stream',
     })) {
-      console.log(res);
+      console.log(res)
     }
-  };
+  }
   const handleRead = () => {
-    read();
-  };
+    read()
+  }
 
   return (
-    <div className={styles.container}>
-      <button onClick={handleSend}>Send</button>
+    <div>
+      <button className="p-10 font-bold" onClick={handleSend}>
+        Send
+      </button>
       <button onClick={handleRead}>Read</button>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
