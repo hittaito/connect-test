ae9f98b04254d53eb7f525e33a1dda5eb815c24a
hittaito-win <310cifra.ggk@gmail.com>
2022-09-11 10:06:31 +0900

    enable cors

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/bufbuild/connect-go v0.4.0
+	github.com/rs/cors v1.8.2
 	golang.org/x/net v0.0.0-20220909164309-bea034e7d591
 	google.golang.org/protobuf v1.28.1
 )
--- server/server.go
+++ server/server.go
@@ -12,9 +12,44 @@ import (
 	"connect-test/chat"
 	"connect-test/generated/schemas/v1/chatconnect"
 
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
+
+func newCORS() *cors.Cors {
+	// To let web developers play with the demo service from browsers, we need a
+	// very permissive CORS setup.
+	return cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodHead,
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+		AllowOriginFunc: func(origin string) bool {
+			// Allow all origins, which effectively disables CORS.
+			return true
+		},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{
+			// Content-Type is in the default safelist.
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Post",
+			"Connect-Accept-Encoding",
+			"Connect-Content-Encoding",
+			"Content-Encoding",
+			"Grpc-Accept-Encoding",
+			"Grpc-Encoding",
+			"Grpc-Message",
+			"Grpc-Status",
+			"Grpc-Status-Details-Bin",
+		},
+	})
+}
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -25,16 +60,15 @@ func main() {
 	mux := http.NewServeMux()
 	path, handler := chatconnect.NewChatServiceHandler(service)
 	mux.Handle(path, handler)
-	// mux.Handle("/", )
 
 	server := &http.Server{
-		Addr:    ":8888",
-		Handler: h2c.NewHandler(mux, &http2.Server{}),
+		Addr:    ":8080",
+		Handler: h2c.NewHandler(newCORS().Handler(mux), &http2.Server{}),
 	}
 
 	go func() {
 		<-ctx.Done()
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		server.Shutdown(ctx)
 	}()
