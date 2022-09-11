a26c9298c335525162bb4d30d9382b1e80db7087
hittaito-win <310cifra.ggk@gmail.com>
2022-09-11 10:05:35 +0900

    frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      extensionAlias: {
+        ".js": [".ts", ".js"],
+      },
+    };
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/schemas.v1.ChatService/:slug",
+        destination: "http://localhost:8080/schemas.v1.ChatService/:slug",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
